f173bae9f1868891e2ec6f9a73aafeb678bcbecd
Mullins, Benjamin <benm@umich.edu>
2025-11-25 17:09:59 -0500

    baby chix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 import TestComponent from "./components/TestComponent";
 
 const Comp = () => {
@@ -7,24 +8,21 @@ const Comp = () => {
 }
 
 function App() {
+  const [count, setCount] = useState(2);
+
   return (
     <div className="App" id='root'>
             <TestComponent />
 
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <Comp />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <button
+        onClick={() => setCount(count + 1)}
+      >Click mE!</button>
+        <div>{'🐥'.repeat(count)}</div>
       </header>
     </div>
   );
